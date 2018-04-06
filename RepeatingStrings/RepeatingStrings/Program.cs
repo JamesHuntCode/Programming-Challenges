bdba13cb2b9ef6ab1eb93547c2606b9577fafbd3
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-04-06 17:44:46 +0100

    setting up - first repeating string in array

---
+++ RepeatingStrings/RepeatingStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatingStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = { "12", "11", "9", "1", "12", "2", "8", "9", "7", "2", "13" };
+
+            bool repeatDetected = DetectRepeatingString(input);
+
+            if (repeatDetected)
+            {
+                Console.WriteLine("First repeating string is: ");
+            }
+            else
+            {
+                Console.WriteLine("No repeating strings have been located within the input collection.");
+            }
+        }
+
+        static bool DetectRepeatingString(string[] collection)
+        {
+            return false;
+        }
+    }
+}
