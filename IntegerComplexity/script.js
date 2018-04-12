e334267a88051ed84b0134c081f800d57b27b076
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-04-12 17:22:43 +0100

    finishing setup - designing algorithm

--- IntegerComplexity/script.js
+++ IntegerComplexity/script.js
@@ -1,1 +1,11 @@
-console.log("setting up...");
+function main() {
+    let inputInt = 12;
+    let sum = getFactorSum(inputInt);
+    console.log("With an input of " + inputInt + ", your answer is " + sum + ".");
+}
+main();
+
+function getFactorSum(n)
+{
+    return n; // come back here...
+}
